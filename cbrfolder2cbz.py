c4b29aeb4b068930dba0b0b822faab6fae2fc917
Nicolas Morey-Chaisemartin <NMoreyChaisemartin@suse.com>
2018-06-19 17:04:12 +0200

    * cbrfolder2cbz.py: Support converting multiple directories at once
    Signed-off-by: Nicolas Morey-Chaisemartin <NMoreyChaisemartin@suse.com>

--- cbrfolder2cbz.py
+++ cbrfolder2cbz.py
@@ -13,7 +13,7 @@ def print_err(message):
     
 def print_usage():
     usage="cbr2cbz A script utility to convert a directory with CBR files into CBZ files"
-    usage+="\n\t Usage: cbr2cbz <directory>"
+    usage+="\n\t Usage: cbr2cbz <directory> [<directory> <directory> ...]"
     usage+="\n\t\n\t (Please don't use non-free formats like RAR/CBR)"
     print_err(usage)
 
@@ -22,16 +22,17 @@ def convert(filename):
     command=" ".join([CONVERT, "\""+filename+"\""])
     execute(command)
 
-if len(sys.argv)!=2:
+if len(sys.argv)<2:
     print_usage();
     sys.exit(-1)
 
 
+sys.argv.pop(0)
 
-
-directory=os.path.abspath(sys.argv[1])
-
-for f in os.listdir(directory):
-    filename_with_path=os.path.join(directory, f)
-    print(filename_with_path)
-    convert(filename_with_path)
+for dir in sys.argv:
+    print dir
+    directory=os.path.abspath(dir)
+    for f in os.listdir(directory):
+        filename_with_path=os.path.join(directory, f)
+        print(filename_with_path)
+        convert(filename_with_path)
